0cf72c5edb6ab849a60a49ad8df958181d30606e
Ikem Nomso <ikemnomso@gmail.com>
2022-11-08 21:42:01 +0100

    updated tests

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -2,7 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByTestId('learn-link');
+	expect(linkElement).toBeInTheDocument();
+});
+
+test('link correctness', () => {
+	render(<App />);
+	const linkElement = screen.getByTestId('learn-link');
+	expect(linkElement.href).toContain('ikemnomso.netlify.app');
 });
